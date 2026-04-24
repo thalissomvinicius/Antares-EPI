7b7f9a1069786eefd92cce629ca98ff0a31e6621
Thalissom Vinicius Souza Cruz <thalissom.cruz@VALLE.br>
2026-04-24 09:59:35 -0300

    fix: resolve bug de sobreposicao (z-index) do cabecalho com modais

--- src/components/layout/ClientShell.tsx
+++ src/components/layout/ClientShell.tsx
@@ -27,7 +27,7 @@ export function ClientShell({ children }: { children: React.ReactNode }) {
         <Sidebar />
         <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
           <Header />
-          <main className="flex-1 overflow-y-auto pb-16 md:pb-0 relative z-0">
+          <main className="flex-1 overflow-y-auto pb-16 md:pb-0">
             {children}
           </main>
         </div>
